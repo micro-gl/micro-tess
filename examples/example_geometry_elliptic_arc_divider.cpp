9724d0110df5a9ec315c523a2e8c4a9787caa0b7
tomer <tomer.shalev@gmail.com>
2021-11-17 10:55:37 +0200

    updated examples

--- examples/example_geometry_elliptic_arc_divider.cpp
+++ examples/example_geometry_elliptic_arc_divider.cpp
@@ -5,19 +5,14 @@
 #include <microgl/bitmaps/bitmap.h>
 #include <microgl/pixel_coders/RGB888_PACKED_32.h>
 #include <microgl/samplers/flat_color.h>
+
 #include <micro-tess/elliptic_arc_divider.h>
 #include <micro-tess/static_array.h>
+#include <micro-tess/dynamic_array.h>
 #include <vector>
 
-#define W 640*1
-#define H 480*1
-
-
 template<typename item_type>
-using static_arr = static_array<item_type, 100>;
-
-template<typename item_type>
-using container = static_arr<item_type>;
+using container = static_array<item_type, 100>;
 //using container = dynamic_array<item_type>;
 //using container = std::vector<item_type>;
 
@@ -25,66 +20,60 @@ int main() {
 //    using number = float;
     using number = Q<15>;
 
-    using Canvas24= canvas<bitmap<coder::RGB888_PACKED_32>>;
+    // microgl drawing setup START
+    using Canvas24 = canvas<bitmap<coder::RGB888_PACKED_32>>;
     sampling::flat_color<> color_red{{255,0,0,255}};
+    Canvas24 canvas(640, 480);
+    // microgl drawing setup END
 
-    Canvas24 canvas(W, H);
-
-    const auto render_arc_internal = [&](
-            const number& start_angle_rad,
-            const number& end_angle_rad,
-            const number& center_x, const number& center_y,
-            const number& radius_x, const number& radius_y,
-            const number& rotation,
-            uint divisions_count) {
-        container<vertex2<number>> arc_points;
-
-        using ellipse = microtess::elliptic_arc_divider<number, decltype(arc_points)>;
-
+    const auto render_arc_internal = [&]() {
+        // Algorithm START
+        // inputs to algorithm
+        container<vertex2<number>> output_points;
+        number start_angle_rad = math::deg_to_rad(0.0f);
+        number end_angle_rad = math::deg_to_rad(-45.0f);
+        number rotation = math::deg_to_rad(45.0f);
+        number radius_x = 50, radius_y = 75;
+        number center_x = 200, center_y=200;
+        unsigned int divisions_count = 32;
+        const bool anti_clock_wise = false;
+        // define algorithm
+        using ellipse = microtess::elliptic_arc_divider<number, decltype(output_points)>;
+        // compute algorithm
         ellipse::compute(
-                arc_points,
-                center_x,
-                center_y,
-                radius_x,
-                radius_y,
-                rotation,
-                start_angle_rad,
-                end_angle_rad,
-                divisions_count,
-            false
-//                true
+                output_points,    // output points
+                center_x,         // x center of elliptic arc
+                center_y,         // y center of elliptic arc
+                radius_x,         // horizontal radius of elliptic arc
+                radius_y,         // vertical radius of elliptic arc
+                rotation,         // rotation about center
+                start_angle_rad,  // starting angle radians
+                end_angle_rad,    // end angle radians
+                divisions_count,  // how many divisions
+                anti_clock_wise   // anti clock-wise ?
         );
+        // Algorithm END
 
         canvas.clear({255,255,255,255});
-
-        for (uint ix = 0; ix < arc_points.size()-0; ++ix) {
+        for (uint ix = 0; ix < output_points.size() - 0; ++ix) {
             canvas.drawCircle<blendmode::Normal, porterduff::FastSourceOverOnOpaque, true>(
                     color_red, color_red,
-                    arc_points[ix].x,
-                    arc_points[ix].y,
+                    output_points[ix].x,
+                    output_points[ix].y,
                     number(4), number(1),
                     120);
         }
 
         canvas.drawWuLinePath(
                 {0,0,0,255},
-                arc_points.data(),
-                arc_points.size(),
+                output_points.data(),
+                output_points.size(),
                 false);
 
     };
 
-    number start_angle_rad = math::deg_to_rad(0.0f);
-    number end_angle_rad = math::deg_to_rad(-45.0f);
-//    number end_angle_rad = math::deg_to_rad(360.0f);
-    number rotation = math::deg_to_rad(45.0f);
-    number radius_x = 50, radius_y = 75;
-    number center_x = 200, center_y=200;
-
     auto render = [&](void*, void*, void*) -> void {
-        render_arc_internal(start_angle_rad, end_angle_rad,
-                center_x, center_y, radius_x, radius_y,
-                rotation,33);
+        render_arc_internal();
     };
 
     example_run(&canvas, render);
